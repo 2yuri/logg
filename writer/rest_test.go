67493f50fdbbe08119d41e4fc866c583448b8cd0
Alexandre Awadallak <alexandre.awadallak@gmail.com>
2021-10-29 10:49:24 -0300

    feat: http mock test

---
+++ writer/rest_test.go
@@ -0,0 +1,53 @@
+package writer
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hyperyuri/logg"
+	"github.com/hyperyuri/logg/levels"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	PROJECT_STACK = "Logger"
+	PROJECT_NAME  = "logg"
+	LOG_MESSAGE   = "Testing rest"
+)
+
+func Test_restW_Write(t *testing.T) {
+
+	body := struct {
+		Message string `json:"message,omitempty"`
+		Stack   string `json:"stack,omitempty"`
+		App     string `json:"app,omitempty"`
+	}{}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		assert.NotNil(t, b)
+		assert.NoError(t, err)
+
+		err = json.Unmarshal(b, &body)
+		assert.NoError(t, err)
+		assert.NotNil(t, body)
+
+		assert.Equal(t, PROJECT_STACK, body.Stack)
+		assert.Equal(t, PROJECT_NAME, body.App)
+		assert.Equal(t, LOG_MESSAGE, body.Message)
+
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	server := httptest.NewServer(handler)
+
+	logger := logg.NewLogger().
+		WithWriter(WithRestApiMode(server.URL, nil, 201)).
+		WithDefaultConfig(logg.NewDefaultConfig(PROJECT_STACK, PROJECT_NAME))
+	logger.SetLevels(levels.NewLevels(logger))
+
+	logger.Info(LOG_MESSAGE)
+}
